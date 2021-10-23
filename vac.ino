d378cd79133925a32f5286bfc155a36c8d2b0e92
Gary <pointer2null@gmail.com>
2021-10-23 19:20:53 +0100

    added the serial library and handler.
    remembered how shit c is for programming.

--- vac.ino
+++ vac.ino
@@ -1,51 +1,121 @@
-#include <SendOnlySoftwareSerial.h>
+#include <Arduino.h>
+#include <SerialCommands.h>
+#include <SoftwareSerial.h>
+#include <EEPROM.h>
 
-int blinkPin = 0;  // pin 5, PB0
-int relay    = 1;  // pin 6, PB1
-int serialTX = 3;  // pin 2, PB3
-int sense    = A1; // pin 7, PB2, ADC1
 
-int analogValue       = 0;
+#define blinkPin 0                   // pin 5, PB0
+#define relay    1                   // pin 6, PB1
+#define serialTX 3                   // pin 2, PB3
+#define serialRX 4                   // pin 3, PB4
+#define sense    A1                  // pin 7, PB2, ADC1
 
-int thresholdRate     = 2;   // how fast we climb when over the trigger level
-int threshold         = 600; // ADC trigger level
-int thresholdCount    = 0;   // counter
-int thresholdCountMax = 10;  // max counter value - affects how long we stay on after the tool is off
-int triggerLevel      = 6;   // counter trigger level that turns on vac
+#define defaultThresholdRate     2   // how fast we climb when over the trigger level
+#define defaultThreshold         600 // ADC trigger level
+#define defaultThresholdCountMax 10  // max counter value - affects how long we stay on after the tool is off
+#define defaultTriggerLevel      6   // count point to switch on
+
+int analogValue       = 0;           // hall sensor value
+int thresholdCount    = 0;           // counter       
 
 boolean blink = false;
+boolean watch = false;
 
-SendOnlySoftwareSerial serial(serialTX);
+char serial_command_buffer_[32];
+struct Data {
+  char magic[1];                    // need some way to see if the eeprom has been initialized
+  int thresholdRate;                // how fast we climb when over the current trigger level
+  int threshold;                    // ADC current trigger level
+  int thresholdCountMax;            // max counter value - affects how long we stay on after the tool is off
+  int triggerLevel;                 // counter trigger level that turns on vac
+};
 
-void setup()
-{
+Data d;
+
+
+SoftwareSerial serial(serialRX, serialTX);
+
+SerialCommands serial_commands_(&serial, serial_command_buffer_, sizeof(serial_command_buffer_), "\r\n", " ");
+
+
+//This is the default handler, and gets called when no other command matches. 
+void cmd_unrecognized(SerialCommands* sender, const char* cmd){
+  watch = false;
+  sender->GetSerial()->print("Unrecognized command [");
+  sender->GetSerial()->print(cmd);
+  sender->GetSerial()->println("]");
+}
+
+//called for set command
+void cmd_set(SerialCommands* sender){
+  watch = false;
+}
+
+//called for get command
+void cmd_get(SerialCommands* sender){
+  watch = false;
+}
+
+//called for watch command
+void cmd_watch(SerialCommands* sender){
+  watch = true;
+}
+
+//Note: Commands are case sensitive
+SerialCommand cmd_set_("set", cmd_set);
+SerialCommand cmd_get_("get", cmd_get);
+SerialCommand cmd_watch_("watch", cmd_watch);
+
+char magicChar[1] = {'G'};               // Magic char  
+
+void setup(){
   serial.begin(9600);
-  serial.print("\n\n\n\r");
+  serial.print("\r\n GVac switch. \r\n");
+  
   pinMode(sense, INPUT);
   pinMode(blinkPin, OUTPUT);
   pinMode(relay, OUTPUT);
+  serial_commands_.SetDefaultHandler(cmd_unrecognized);
+  serial_commands_.AddCommand(&cmd_set_);
+  serial_commands_.AddCommand(&cmd_get_);
+  serial_commands_.AddCommand(&cmd_watch_);
+  // check eeprom - see if we've been initialized before
+  
+  EEPROM.get(0, d);
+  if (d.magic[0] != 'G'){
+    serial.print("Using defaults.\r\n");
+    // this is the first ever run
+    // set defaults
+    d.magic[0]          = magicChar;
+    d.thresholdCountMax = defaultThresholdCountMax;
+    d.triggerLevel      = defaultTriggerLevel;
+    d.thresholdRate     = defaultThresholdRate;
+    d.threshold         = defaultThreshold;
+    EEPROM.put(0, d);
+  }
 }
 
-void loop()
-{
+void loop(){
   analogValue = analogRead(sense);
-  if ((analogValue >= threshold) &&
-      (thresholdCount <= thresholdCountMax)) {
+  if ((analogValue >= d.threshold) &&
+      (thresholdCount <= d.thresholdCountMax)) {
     // count up fast
-    thresholdCount += thresholdRate;
+    thresholdCount += d.thresholdRate;
     blink = true;
   } else {
     blink = false;
   }
-  if ((analogValue < threshold) && (thresholdCount > 0 )) {
+  if ((analogValue < d.threshold) && (thresholdCount > 0 )) {
     // count down slow
     thresholdCount--;
   }
-  serialDump(analogValue, thresholdCount);
-  if (thresholdCount > triggerLevel) {
+  if (watch) {
+    valuesDump();
+  }
+  if (thresholdCount > d.triggerLevel) {
     digitalWrite(blinkPin, HIGH);
     digitalWrite(relay, HIGH);
-    
+    blink = false;
   } else {
     digitalWrite(relay, LOW);
     digitalWrite(blinkPin, LOW);
@@ -60,13 +130,10 @@ void loop()
   } else {
     delay(1000);
   }
-
+  serial_commands_.ReadSerial();
 }
 
-void serialDump(int val, int th) {
-  serial.print("\r                               ");
-  serial.print("\rValue = ");
-  serial.print(val, DEC);
-  serial.print(" ThCnt = ");
-  serial.print(th, DEC);
+void valuesDump() {
+  serial.print("\r\nValue = ");
+  serial.print(analogValue, DEC);
 }
